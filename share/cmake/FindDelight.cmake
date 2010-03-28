0af807b70c1c18d0e4ac02e1c3985d823a1eaa1b
Dan Bethell <dan@samus.(none)>
2010-03-28 14:08:16 +1100

    Added PRMan cmake module and renamed 3Delight to Delight as cmake has problems with conditionals and variables that start with a number. Consequently all the 3Delight_ variables are now just Delight.

--- share/cmake/FindDelight.cmake
+++ share/cmake/FindDelight.cmake
@@ -34,50 +34,50 @@
 #==========
 #
 # Variables defined by this module:
-#   3Delight_FOUND    
-#   3Delight_INCLUDE_DIR
-#   3Delight_COMPILE_FLAGS
-#   3Delight_LIBRARIES
-#   3Delight_LIBRARY_DIR
+#   Delight_FOUND    
+#   Delight_INCLUDE_DIR
+#   Delight_COMPILE_FLAGS
+#   Delight_LIBRARIES
+#   Delight_LIBRARY_DIR
 #
 # Usage: 
-#   FIND_PACKAGE( 3Delight )
-#   FIND_PACKAGE( 3Delight REQUIRED )
+#   FIND_PACKAGE( Delight )
+#   FIND_PACKAGE( Delight REQUIRED )
 #
 # Note:
-# You can tell the module where 3Delight is installed by setting
-# the 3Delight_INSTALL_PATH (or setting the DELIGHT environment
+# You can tell the module where Delight is installed by setting
+# the Delight_INSTALL_PATH (or setting the DELIGHT environment
 # variable before calling FIND_PACKAGE.
 # 
 # E.g. 
-#   SET( 3Delight_INSTALL_PATH "/usr/local/3delight-9.0.0/Linux-x86_64" )
-#   FIND_PACKAGE( 3Delight REQUIRED )
+#   SET( Delight_INSTALL_PATH "/usr/local/3delight-9.0.0/Linux-x86_64" )
+#   FIND_PACKAGE( Delight REQUIRED )
 #
 #==========
 
 # our includes
-FIND_PATH( 3Delight_INCLUDE_DIR ri.h
+FIND_PATH( Delight_INCLUDE_DIR ri.h
   $ENV{DELIGHT}/include
-  ${3Delight_INSTALL_PATH}/include
+  ${Delight_INSTALL_PATH}/include
   )
 
 # our compilation flags
-SET( 3Delight_COMPILE_FLAGS "-DDELIGHT" )
+SET( Delight_COMPILE_FLAGS "-DDELIGHT" )
 
 # our library itself
-FIND_LIBRARY( 3Delight_LIBRARIES 3delight
+FIND_LIBRARY( Delight_LIBRARIES 3delight
   $ENV{DELIGHT}/lib
-  ${3Delight_INSTALL_PATH}/lib
+  ${Delight_INSTALL_PATH}/lib
   )
 
 # our library path
-GET_FILENAME_COMPONENT( 3Delight_LIBRARY_DIR ${3Delight_LIBRARIES} PATH )
+GET_FILENAME_COMPONENT( Delight_LIBRARY_DIR ${Delight_LIBRARIES} PATH )
 
 # did we find everything?
 INCLUDE( FindPackageHandleStandardArgs )
-FIND_PACKAGE_HANDLE_STANDARD_ARGS( "3Delight" DEFAULT_MSG
-  3Delight_INCLUDE_DIR
-  3Delight_COMPILE_FLAGS
-  3Delight_LIBRARIES
-  3Delight_LIBRARY_DIR
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( "Delight" DEFAULT_MSG
+  Delight_INCLUDE_DIR
+  Delight_COMPILE_FLAGS
+  Delight_LIBRARIES
+  Delight_LIBRARY_DIR
   )
